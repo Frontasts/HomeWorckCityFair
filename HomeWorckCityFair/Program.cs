36ca505f7f3f671478d068105552af4480300e28
Frontasts <steamfronts0110@gmail.com>
2026-07-12 14:23:04 +0300

    Main: добавлена демонстрация работы классов и методов

--- HomeWorckCityFair/Program.cs
+++ HomeWorckCityFair/Program.cs
@@ -10,6 +10,60 @@ namespace HomeWorckCityFair
     {
         public static void Main()
         {
+            Vendor vendor1 = new Vendor("Анна Смирнова");
+            Vendor vendor2 = new Vendor("Борис Козлов");
+            Vendor vendor3 = new Vendor("Виктория Новикова");
+
+            Stall stallCenter = new Stall("Центральная площадь");
+            Stall stallPark = new Stall("Городской парк");
+            Stall stallMarket = new Stall("Рынок на востоке");
+
+            Console.WriteLine(stallCenter.AssignVendor(vendor1));
+            Console.WriteLine(stallCenter.AssignVendor(vendor2));
+
+            Console.WriteLine(stallPark.AssignVendor(vendor2));
+            Console.WriteLine(stallPark.AssignVendor(vendor3));
+
+            Console.WriteLine(stallMarket.AssignVendor(vendor1));
+            Console.WriteLine(stallMarket.AssignVendor(vendor3));
+            Console.WriteLine();
+
+            Fair fair = new Fair();
+            Console.WriteLine(fair.AddStall(stallCenter));
+            Console.WriteLine(fair.AddStall(stallPark));
+            Console.WriteLine(fair.AddStall(stallMarket));
+
+            Stall[] stalls = fair.GetAllStalls();
+            Console.WriteLine();
+
+            foreach (Stall stall in stalls)
+            {
+                Console.WriteLine(stall.GetLocation());
+                Vendor[] vendors = stall.GetAllVendor();
+                foreach (Vendor vendor in vendors)
+                {
+                    Console.WriteLine(vendor.GetName());
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("--- Борис покидает ярмарку ---");
+            stallCenter.RemoveVendor(vendor2);
+            stallPark.RemoveVendor(vendor2);
+
+            stalls = fair.GetAllStalls();
+            Console.WriteLine();
+
+            foreach (Stall stall in stalls)
+            {
+                Console.WriteLine(stall.GetLocation());
+                Vendor[] vendors = stall.GetAllVendor();
+                foreach (Vendor vendor in vendors)
+                {
+                    Console.WriteLine(vendor.GetName());
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
