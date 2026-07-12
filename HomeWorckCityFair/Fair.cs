ca681f6f08b8e0ae285dc9ffd8e520f08856c779
Frontasts <steamfronts0110@gmail.com>
2026-07-12 14:02:54 +0300

    Fair: добавлено поле AddStall() и гетер GetAllStalls()

--- HomeWorckCityFair/Fair.cs
+++ HomeWorckCityFair/Fair.cs
@@ -14,5 +14,25 @@ namespace HomeWorckCityFair
         {
             _stalls = new List<Stall>();
         }
+
+        public bool AddStall(Stall stall)
+        {
+            if(stall == null)
+            {
+                return false;
+            }
+
+            _stalls.Add(stall);
+            return true;
+        }
+
+        public Stall[] GetAllStalls()
+        {
+            int stallLenght = _stalls.Count();
+            Stall[] stalls = new Stall[stallLenght];
+
+            _stalls.CopyTo(stalls);
+            return stalls;
+        }
     }
 }
